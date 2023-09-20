65a6864fc1a6da10b2f2555bcae6882434b2aeab
saluumaa <salumahassan02@gmail.com>
2023-09-20 12:13:02 +0300

    Add components and redux store

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Greetings from './components/Greetings';
+
+function App() {
+  return (
+    // <Router>
+    //     <Routes>
+    //         <Route exact path="/" component={Greetings} />
+    //     </Routes>
+    // </Router>
+    <Greetings />
+  );
+}
+
+export default App;
